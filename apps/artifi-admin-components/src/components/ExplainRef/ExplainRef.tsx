9ea98efec7ae5a53e521fc6993a4bfffd31bbf46
AMLA\nidhi.nimje <nidhi.nimje@amla.io>
2020-11-12 19:31:07 +0530

    Code Review Points done

--- apps/artifi-admin-components/src/components/ExplainRef/ExplainRef.tsx
+++ apps/artifi-admin-components/src/components/ExplainRef/ExplainRef.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 export function ExplainRef() {
-  const firstNameRef = React.createRef();
-  const lastNameRef = React.createRef();
-  const fullNameRef = React.createRef();
+  const firstNameRef = React.createRef<HTMLInputElement>();
+  const lastNameRef = React.createRef<HTMLInputElement>();
+  const fullNameRef = React.createRef<HTMLLabelElement>();
 
   const OnSubmit = (e) => {
+    //if(fullNameRef.current){
     fullNameRef.current.innerHTML =
       firstNameRef.current.value + " " + lastNameRef.current.value;
+    //}
   };
 
   return (
--- apps/artifi-admin-components/src/index.tsx
+++ apps/artifi-admin-components/src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { TextInput } from "./components/InputText";
+import { TextInput } from "./components/InputText/InputText";
 import { ExplainRef } from './components/ExplainRef/ExplainRef';
 
 export function App() {
